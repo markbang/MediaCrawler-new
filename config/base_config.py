2a3db6a39f9288f7029dccd299fd89a91d1a1064
markbang <3137972908@qq.com>
2024-03-29 19:40:59 +0800

    update

--- config/base_config.py
+++ config/base_config.py
@@ -2,7 +2,7 @@
 PLATFORM = "xhs"
 KEYWORDS = "python,golang"
 LOGIN_TYPE = "qrcode"  # qrcode or phone or cookie
-COOKIES = ""
+COOKIES = "passport_assist_user=CkBB6P2L5JMx_Ml02htqjzK2h5bXs0vEY1e-sRhYerUCy0QrPk3GEJBlvMXCGLR4gH5WAk9ZttaKrisPNdarwT33GkoKPDRFxzE_tg2MjOCYXWB2V2D82FaJra3qHtVrT7sJUKDjNPAKonfHSB0Kk7VPlFegHZayy_tK66bwE7Po_hDL0r4NGImv1lQgASIBA_qJPHg%3D; sso_uid_tt=84a85b65ef441e2d323fca7661773326; sso_uid_tt_ss=84a85b65ef441e2d323fca7661773326; toutiao_sso_user=657e7d8a3be4f27b188bd4432fdda358; toutiao_sso_user_ss=657e7d8a3be4f27b188bd4432fdda358; LOGIN_STATUS=1; store-region=cn-sh; store-region-src=uid; d_ticket=5a96f7076b4783a73771efadded4fafe744ab; bd_ticket_guard_client_web_domain=2; my_rd=2; _bd_ticket_crypt_cookie=449041550fca1a8fec0d6e8c4224119d; post_dropdown_capcut_link=true; uid_tt=fb9e3badb5a32ad471bd9fab901eed26; uid_tt_ss=fb9e3badb5a32ad471bd9fab901eed26; sid_tt=7bbffdaea3f9e9b90c05cacf6f0c2729; sessionid=7bbffdaea3f9e9b90c05cacf6f0c2729; sessionid_ss=7bbffdaea3f9e9b90c05cacf6f0c2729; live_use_vvc=%22false%22; dy_sheight=915; sid_ucp_sso_v1=1.0.0-KGUzM2U3Y2JhZTFmMGQ3ZWFmZTE2OTc5ODBmMTUzNWYyNDNlNDg3NDYKHgjr9ICUx_Q7EI_-oa8GGO8xIAww6d-c-gU4BkD0BxoCbHEiIDY1N2U3ZDhhM2JlNGYyN2IxODhiZDQ0MzJmZGRhMzU4; ssid_ucp_sso_v1=1.0.0-KGUzM2U3Y2JhZTFmMGQ3ZWFmZTE2OTc5ODBmMTUzNWYyNDNlNDg3NDYKHgjr9ICUx_Q7EI_-oa8GGO8xIAww6d-c-gU4BkD0BxoCbHEiIDY1N2U3ZDhhM2JlNGYyN2IxODhiZDQ0MzJmZGRhMzU4; sid_guard=7bbffdaea3f9e9b90c05cacf6f0c2729%7C1709735695%7C5184000%7CSun%2C+05-May-2024+14%3A34%3A55+GMT; sid_ucp_v1=1.0.0-KDNjZDllZjRjNjllYWJlNzY2ZTliMWQxN2IxZDIxOWZmNWNhNTA2NTkKGgjr9ICUx_Q7EI_-oa8GGO8xIAw4BkD0B0gEGgJscSIgN2JiZmZkYWVhM2Y5ZTliOTBjMDVjYWNmNmYwYzI3Mjk; ssid_ucp_v1=1.0.0-KDNjZDllZjRjNjllYWJlNzY2ZTliMWQxN2IxZDIxOWZmNWNhNTA2NTkKGgjr9ICUx_Q7EI_-oa8GGO8xIAw4BkD0B0gEGgJscSIgN2JiZmZkYWVhM2Y5ZTliOTBjMDVjYWNmNmYwYzI3Mjk; passport_csrf_token=6f30d63a9370b67083585944b6eac491; passport_csrf_token_default=6f30d63a9370b67083585944b6eac491; xgplayer_device_id=96671122209; xgplayer_user_id=858036523386; download_guide=%222%2F20240310%2F1%22; dy_swidth=1463; __live_version__=%221.1.1.8649%22; volume_info=%7B%22isUserMute%22%3Afalse%2C%22isMute%22%3Afalse%2C%22volume%22%3A1%7D; ttwid=1%7Cyvb8ZVDuiRn-L7RRwdaKj8vLjmFlq2fh0vSTPc2Ipc4%7C1710682184%7Cf4498e40dce148421c3a406746af69a13112957ef42304124940fc7e3e74ac0e; stream_player_status_params=%22%7B%5C%22is_auto_play%5C%22%3A0%2C%5C%22is_full_screen%5C%22%3A0%2C%5C%22is_full_webscreen%5C%22%3A0%2C%5C%22is_mute%5C%22%3A0%2C%5C%22is_speed%5C%22%3A1%2C%5C%22is_visible%5C%22%3A1%7D%22; live_can_add_dy_2_desktop=%221%22; douyin.com; device_web_cpu_core=16; device_web_memory_size=8; architecture=amd64; csrf_session_id=3067947596559e0957b77b3d190d365b; strategyABtestKey=%221711250289.741%22; publish_badge_show_info=%220%2C0%2C0%2C1711250290637%22; FRIEND_NUMBER_RED_POINT_INFO=%22MS4wLjABAAAA_K4gfwzzCaivurJFSGFDS6i80Bz-lpm4P-TGlZm-Gjk%2F1711296000000%2F1711250753355%2F0%2F0%22; xg_device_score=7.6233091352684825; FOLLOW_LIVE_POINT_INFO=%22MS4wLjABAAAA_K4gfwzzCaivurJFSGFDS6i80Bz-lpm4P-TGlZm-Gjk%2F1711296000000%2F0%2F0%2F1711251353623%22; __ac_nonce=065ffa8e30029dbc9cbb; __ac_signature=_02B4Z6wo00f01LPUQywAAIDCPfxphsAedYCz9EeAAEkPwFdLeSoP9JBl7ShdqVS6ajDOjU1phwE9annG7ZoEXXTtT.zm85wig9FflNSQiYkk6wFSr3egAE86xzPZrXN9y.J7T25tIIkv6PaGdd; FOLLOW_NUMBER_YELLOW_POINT_INFO=%22MS4wLjABAAAA_K4gfwzzCaivurJFSGFDS6i80Bz-lpm4P-TGlZm-Gjk%2F1711296000000%2F0%2F0%2F1711254919395%22; tt_scid=MlBRGFJIoRS7B77UJEcU03gJsswpKHRmPLsqFvDQa5uc8ycXbKIZX2Eatt9coWTm9310; pwa2=%220%7C0%7C2%7C1%22; odin_tt=404f1b5510cd2356b32f512c4116db5bb84d1b869f34b9d60d2138764a104639e2af1a9e14ef56fed9899d8225e9362feaec067fa9a733dafbfb51a827f2b548; s_v_web_id=verify_lu50kngi_tnFMhzDO_xYTz_4U4k_BYca_rEdjpGggCEsF; msToken=3IrWQNkKFqymixOsga3UidT_aHX4lWXEuxs7lc-iingy7r7YG7q3TjNA1PrisqrJpfzHN70f4V1JwS41nTkHrblNqOMmZXrGzbToN2-qmaevT0Z4PDkToPmjVk-G; IsDouyinActive=true; stream_recommend_feed_params=%22%7B%5C%22cookie_enabled%5C%22%3Atrue%2C%5C%22screen_width%5C%22%3A1463%2C%5C%22screen_height%5C%22%3A915%2C%5C%22browser_online%5C%22%3Atrue%2C%5C%22cpu_core_num%5C%22%3A16%2C%5C%22device_memory%5C%22%3A8%2C%5C%22downlink%5C%22%3A4.65%2C%5C%22effective_type%5C%22%3A%5C%224g%5C%22%2C%5C%22round_trip_time%5C%22%3A50%7D%22; msToken=2nmfmMjwDpu-RccPwy8GyWt8XGHbhRqGmEhghkcCGekvDhobdoLSpYbQrUFtq-l0gKoXm_FDxr5NaqLsB_vbOB_MGj9NTwB_vp2O6uZicefUQRPiQEWpdyAquDmu; home_can_add_dy_2_desktop=%221%22; bd_ticket_guard_client_data=eyJiZC10aWNrZXQtZ3VhcmQtdmVyc2lvbiI6MiwiYmQtdGlja2V0LWd1YXJkLWl0ZXJhdGlvbi12ZXJzaW9uIjoxLCJiZC10aWNrZXQtZ3VhcmQtcmVlLXB1YmxpYy1rZXkiOiJCQjZmT3BVRHREbmFRNUpDQ1gwNmliSnZNcHFDeFpkTkRRYlRmYjBOK3BXSmxEYjlPTEl1MmR3RGVRZGdJVG84YVpuRGllcWxyMXIrSWNybnYyZWdieUE9IiwiYmQtdGlja2V0LWd1YXJkLXdlYi12ZXJzaW9uIjoxfQ%3D%3D; passport_fe_beating_status=true"
 SORT_TYPE = "popularity_descending"  # 具体值参见media_platform.xxx.field下的枚举值，展示只支持小红书
 CRAWLER_TYPE = "search" # 爬取类型，search(关键词搜索) | detail(帖子相亲)| creator(创作者主页数据) | video_download (视频下载暂时只支持 bili)
 
@@ -19,7 +19,7 @@ HEADLESS = True
 SAVE_LOGIN_STATE = True
 
 # 数据保存类型选项配置,支持三种类型：csv、db、json
-SAVE_DATA_OPTION = "json"  # csv or db or json
+SAVE_DATA_OPTION = "csv"  # csv or db or json
 
 # 用户浏览器缓存的浏览器文件配置
 USER_DATA_DIR = "%s_user_data_dir"  # %s will be replaced by platform name
@@ -31,7 +31,7 @@ CRAWLER_MAX_NOTES_COUNT = 20
 MAX_CONCURRENCY_NUM = 4
 
 # 是否开启爬评论模式, 默认不开启爬评论
-ENABLE_GET_COMMENTS = False
+ENABLE_GET_COMMENTS = True
 
 # 指定小红书需要爬虫的笔记ID列表
 XHS_SPECIFIED_ID_LIST = [
@@ -43,15 +43,13 @@ XHS_SPECIFIED_ID_LIST = [
 
 # 指定抖音需要爬取的ID列表
 DY_SPECIFIED_ID_LIST = [
-    "7280854932641664319",
-    "7202432992642387233"
+    "7350630104407575860",
     # ........................
 ]
 
 # 指定快手平台需要爬取的ID列表
 KS_SPECIFIED_ID_LIST = [
-    "3xf8enb8dbj6uig",
-    "3x6zz972bchmvqe"
+    "3x9kd9quqa2ecgg",
 ]
 
 # 指定B站平台需要爬取的视频bvid列表
